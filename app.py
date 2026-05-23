d516c1e5cfa0a5e12de81c7f39990108732d3d07
Heckler Ochoa Caicedo <71579192+heckler74@users.noreply.github.com>
2026-05-23 09:48:48 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 import streamlit as st
 import pandas as pd
 import libreria_funciones as lf
-import librería_clase as lc
+import librería_clases as lc
 
 st.title("Proyecto final UCG")
 
 st.sidebar.title("Parámetros")
 
 st.sidebar.image("Python_logo.png")
-modulo = st.sidebar.selectbox("Seleccione un módulo",["Módulo 1","Módulo 2","Módulo 3"])
-if  modulo== "Módulo 1":
+
+
+modulo = st.sidebar.selectbox("Seleccione un módulo", ["Módulo 1", "Módulo 2", "Módulo 3"] )
+
+if modulo == "Módulo 1":
+
     uploaded_files = st.file_uploader(
         "Upload data", accept_multiple_files=True, type="csv"
     )
     for uploaded_file in uploaded_files:
         df = pd.read_csv(uploaded_file)
         st.write(df)
-elif modulo=="Módulo 2":
+
+elif modulo == "Módulo 2":
     monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
     interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
     anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
@@ -24,3 +29,6 @@ elif modulo=="Módulo 2":
     
     cuota = lf.cuota_prestamo(monto, interes,anios,numero_pagos)
     st.write("Su cuota mensual es: ",cuota)
+
+else:
+    st.write("Se encuentra en el módulo 3")
